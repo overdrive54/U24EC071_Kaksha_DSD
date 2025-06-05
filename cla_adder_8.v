42a6132c0b01f86fba3f1757a840efb13b040556
Kaksha Subhashbhai Makwana <variantloki54@gmail.com>
2025-06-05 10:09:17 +0530

    Create cla_adder_8.v

---
+++ cla_adder_8.v
@@ -0,0 +1,62 @@
+// Code your design here
+module full_adder (
+    input   a,
+    input   b,
+    input   cin,
+    output  sum
+);
+    assign sum = a ^ b ^ cin;
+endmodule
+
+module cla_generator_8 (
+    input  [7:0] p,
+    input  [7:0] g,
+    input        cin,
+    output [8:0] c );
+    assign c[0] = cin;
+    assign c[1] = g[0] | (p[0] & c[0]);
+    assign c[2] = g[1] | (p[1] & g[0]) | (p[1] & p[0] & c[0]);
+    assign c[3] = g[2] | (p[2] & g[1]) | (p[2] & p[1] & g[0]) | (p[2] & p[1] & p[0] & c[0]);
+    assign c[4] = g[3] | (p[3] & g[2]) | (p[3] & p[2] & g[1]) | (p[3] & p[2] & p[1] & g[0]) | (p[3] & p[2] & p[1] & p[0] & c[0]);
+    assign c[5] = g[4] | (p[4] & g[3]) | (p[4] & p[3] & g[2]) | (p[4] & p[3] & p[2] & g[1]) | (p[4] & p[3] & p[2] & p[1] & g[0]) | (p[4] & p[3] & p[2] & p[1] & p[0] & c[0]);
+    assign c[6] = g[5] | (p[5] & g[4]) | (p[5] & p[4] & g[3]) | (p[5] & p[4] & p[3] & g[2]) | (p[5] & p[4] & p[3] & p[2] & g[1]) | (p[5] & p[4] & p[3] & p[2] & p[1] & g[0]) | (p[5] & p[4] & p[3] & p[2] & p[1] & p[0] & c[0]);
+    assign c[7] = g[6] | (p[6] & g[5]) | (p[6] & p[5] & g[4]) | (p[6] & p[5] & p[4] & g[3]) | (p[6] & p[5] & p[4] & p[3] & g[2]) | (p[6] & p[5] & p[4] & p[3] & p[2] & g[1]) | (p[6] & p[5] & p[4] & p[3] & p[2] & p[1] & g[0]) | (p[6] & p[5] & p[4] & p[3] & p[2] & p[1] & p[0] & c[0]);
+    assign c[8] = g[7] | (p[7] & g[6]) | (p[7] & p[6] & g[5]) | (p[7] & p[6] & p[5] & g[4]) | (p[7] & p[6] & p[5] & p[4] & g[3]) | (p[7] & p[6] & p[5] & p[4] & p[3] & g[2]) | (p[7] & p[6] & p[5] & p[4] & p[3] & p[2] & g[1]) | (p[7] & p[6] & p[5] & p[4] & p[3] & p[2] & p[1] & g[0]) | (p[7] & p[6] & p[5] & p[4] & p[3] & p[2] & p[1] & p[0] & c[0]);
+endmodule
+
+
+module cla_adder_8 (
+    input  [7:0] a,
+    input  [7:0] b,
+    input        cin,
+    output       cout,
+    output [7:0] sum
+);
+    wire [7:0] p, g;
+    assign p = a ^ b;
+    assign g = a & b;
+    
+    wire [8:0] c;
+    
+    cla_generator_8 cla_gen (
+        .p(p),
+        .g(g),
+        .cin(cin),
+        .c(c)
+    );
+    
+    genvar i;
+    generate
+        for (i = 0; i < 8; i = i + 1) begin : full_adder_gen
+            full_adder fa (
+                .a(a[i]),
+                .b(b[i]),
+                .cin(c[i]),
+                .sum(sum[i])
+            );
+        end
+    endgenerate
+    
+    assign cout = c[8];
+    
+endmodule
