bc7a183852fb10fc53db32e3217f2c6fe83c834e
Kaksha Subhashbhai Makwana <variantloki54@gmail.com>
2025-06-08 22:39:32 +0530

    Create nand_gate.v

---
+++ nand_gate.v
@@ -0,0 +1,33 @@
+// Code your design here
+module mux2to1 (
+    input  sel,
+    input  in0,
+    input  in1,
+    output out
+);
+    assign out = sel ? in1 : in0;
+endmodule
+
+module // Code your design here
+module mux2to1 (
+    input  sel,
+    input  in0,
+    input  in1,
+    output out
+);
+    assign out = sel ? in1 : in0;
+endmodule
+
+module nand_gate(input a,
+                input b,
+                output wire y);
+  
+  mux2to1 m1 (.sel(!b), .in0(!a), .in1(1), .out(y));
+  
+endmodule(input a,
+                input b,
+                output wire y);
+  
+  mux2to1 m1 (.sel(!b), .in0(!a), .in1(1), .out(y));
+  
+endmodule
