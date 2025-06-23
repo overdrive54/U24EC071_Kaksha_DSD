31d78682862855bc6dedd1576a9623cd98033c94
Kaksha Subhashbhai Makwana <variantloki54@gmail.com>
2025-06-23 17:50:49 +0530

    Create synch_down_count_4_tb.v

---
+++ COUNTER/SYNCHRONOUS/synch_down_count_4_tb.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+
+module synch_down_count_4_tb;
+
+    reg clk;
+    wire [3:0] q;
+    wire [3:0] qbar;
+
+    synch_down_count_4 uut (
+        .clk(clk),
+        .q(q),
+        .qbar(qbar)
+    );
+
+    initial clk = 0;
+    always #5 clk = ~clk;
+    initial begin
+    clk = 0;
+    uut.t0.q = 0;
+    uut.t1.q = 0;
+    uut.t2.q = 0;
+    uut.t3.q = 0;
+end
+    initial begin
+        $dumpfile("up_count_4_t_ff.vcd");
+        $dumpvars(0, uut);
+        $monitor("Time = %0t | Counter = %b", $time, q);
+
+        #200 $finish;
+    end
+
+endmodule
